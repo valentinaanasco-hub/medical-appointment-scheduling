8d5a80a8552ea17962236eb703ed5c8d0659d98b
valentinaanasco-hub <valentinaanasco@unicauca.edu.co>
2026-02-11 08:24:36 -0500

    Initial commit

---
+++ src/main/java/com/mycompany/agendamientocitasmedicas/AgendamientoCitasMedicas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.agendamientocitasmedicas;
+
+/**
+ *
+ * @author valen
+ */
+public class AgendamientoCitasMedicas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
