496d9514bc8280d3b2dbe6a2a42e60588e1e1475
SantiagoGutierrez11 <mariadr@unicauca.edu.co>
2026-03-25 21:33:48 -0500

    Se agrega patron Factory Method (Fabrica: IServiceFactory y su fabrica concreta PostgresServiceFactory) coordinado por PiedrazulFacade

--- src/main/java/co/unicauca/piedrazul/application/PiedrazulFacade.java
+++ src/main/java/co/unicauca/piedrazul/application/PiedrazulFacade.java
@@ -1,10 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package co.unicauca.piedrazul.application;
 
 import co.unicauca.piedrazul.domain.services.interfaces.IServiceFactory;
+import co.unicauca.piedrazul.main.DataBaseType;
+import co.unicauca.piedrazul.main.ServiceFactoryProvider;
 
 /**
  *
@@ -16,6 +14,21 @@ public class PiedrazulFacade {
 
     private PiedrazulFacade(IServiceFactory factory) {
         this.serviceFactory = factory;
+    }
+    
+    /**
+     * Obtiene la instancia única de la fachada.
+     * @param dbType Tipo de base de datos a utilizar.
+     * @return Instancia de PiedrazulFacade.
+     */
+    //Patron singleton
+    public static PiedrazulFacade getInstance(DataBaseType dbType) { 
+        if (instance == null) {
+            // Obtenemos la fábrica concreta a través del Provider
+            IServiceFactory factory = ServiceFactoryProvider.getFactory(dbType);
+            instance = new PiedrazulFacade(factory);
+        }
+        return instance;
     }
     //Controladores
 
